9493c04cccb856609d70bcd5c026d4f84a89d8f3
PriyankaRumal <priyankarumal182000@gmail.com>
2023-06-06 11:13:08 +0530

    [priyanka]:single level Inheritance

---
+++ PracticeFiles/Inheritance/SingleInheritance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeFiles.Inheritance
+{
+    public class SingleInheritance//Employee
+    {
+        public float salary = 40000;
+    }
+    public class Programmer:SingleInheritance
+    {
+        public float bonus = 10000;
+    }
+}
--- PracticeFiles/Program.cs
+++ PracticeFiles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using PracticeFiles.ClassesAndObjects;
+using PracticeFiles.Inheritance;
 using System;
 
 namespace PracticeFiles
@@ -10,9 +11,12 @@ namespace PracticeFiles
             //OopsProgram1 p1 = new OopsProgram1();
             //p1.insert(101, "Priyanka");
             //p1.display();
-            Constructore con=new Constructore();
-            Constructore con2 = new Constructore(101, "Priyanka");
-            con2.display();
+            //Constructore con=new Constructore();
+            //Constructore con2 = new Constructore(101, "Priyanka");
+            //con2.display();
+            Programmer p1 = new Programmer();
+            Console.WriteLine("salary:"+p1.salary);
+            Console.WriteLine("Bounus:" +p1.bonus);
  
         }
     }
